4dcdb45a591922a92f7e1260843829a3f860bb18
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-04-04 12:04:44 -0500

    conf

--- src/Config/axiosconfiguracion.jsx
+++ src/Config/axiosconfiguracion.jsx
@@ -2,5 +2,6 @@ import axios from "axios"
 
 export const axiosconfiguracion = axios.create({
    // baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5500/api'
+   
    baseURL: process.env.REACT_APP_BASE_URL || 'https://movies-backend-m8kh.onrender.com'
 })
