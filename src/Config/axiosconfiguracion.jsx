dfadcb526ce6acebca7c5cd20ce0428d35a1d002
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-04-04 17:13:28 -0500

    nm

--- src/Config/axiosconfiguracion.jsx
+++ src/Config/axiosconfiguracion.jsx
@@ -1,8 +1,6 @@
 import axios from "axios"
 
 export const axiosconfiguracion = axios.create({
-   // baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5500/api'
-   
-   baseURL: process.env.REACT_APP_BASE_URL || 'https://movies-backend-1-bmlf.onrender.com/api/'
-   
-})
+   baseURL: process.env.REACT_APP_BASE_URL || "https://movies-backend-1-belf.onrender.com"
+ });
+ 
